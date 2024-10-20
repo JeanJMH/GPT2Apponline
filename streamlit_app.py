e9c934b493127c78c87ecf0c218216a2a86b7725
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-10-20 11:27:12 -0400

    Test4

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("My Super Awesome OpenAI API Deployment!")
 
 generator = pipeline('text-generation', model='gpt2')
 
-output = generator("Damascus is a", max_length=20, num_return_sequences=1, truncation=True))
+output = generator("Damascus is a", max_length=20, num_return_sequences=1, truncation=True)
 
 st.write(output[0]['generated_text'])
 
