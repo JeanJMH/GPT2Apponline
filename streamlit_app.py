bc396fc92ccedf72161a0a5ec1397837dc22ade8
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-10-19 23:38:21 -0400

    test to use molde ChatGPT2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+### Load your API Key
+my_secret_key = st.secrets['MyOpenAIKey']
+os.environ["OPENAI_API_KEY"] = my_secret_key
+
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt2",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": "Damascus is a"}
+  ],
+  n=10,
+  max_tokens=20
 )
+
+### Print all 10 completions:
+for i in range(10):
+ st.write(response.choices[i].message.content)
