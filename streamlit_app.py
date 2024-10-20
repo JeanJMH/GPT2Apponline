503ea80187d6aa8aa727d74e91edd3deb9b67bfe
Jean Miranda <156374467+JeanJMH@users.noreply.github.com>
2024-10-20 11:35:22 -0400

    Prompt input

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,12 @@ st.title("My Super Awesome OpenAI API Deployment!")
 
 generator = pipeline('text-generation', model='gpt2')
 
-output = generator("Damascus is a", max_length=20, num_return_sequences=1, truncation=True)
+### user input
+
+prompt = st.text_input("Enter your prompt:")
+
+
+output = generator("promt", max_length=20, num_return_sequences=1, truncation=True)
 
 st.write(output[0]['generated_text'])
 
